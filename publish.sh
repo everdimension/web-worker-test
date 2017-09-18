3ff233eac5e2a471691ee0f8347a4cd846d355ee
everdimension <everdimension@gmail.com>
2017-09-18 17:37:55 +0300

    build

--- publish.sh
+++ publish.sh
@@ -1,5 +1,12 @@
 git checkout gh-pages
+
+# copy all files from master branch
 git checkout master -- ./
+
+# we should remove "build" path from .gitignore
+gitignoreContents=$(cat .gitignore)
+echo ${gitignoreContents/build} > .gitignore
+
 npm i
 rm -rf build/*
 npm run build
