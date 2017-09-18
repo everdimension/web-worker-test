998edebd92e12b9950488a7e4bdc0e12e4a22d39
everdimension <everdimension@gmail.com>
2017-09-18 18:08:29 +0300

    update build for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 
 const env = process.env.NODE_ENV;
@@ -5,13 +6,13 @@ const production = env === 'production';
 
 console.log('env is', env, production);
 
-module.exports = {
+const config = {
   entry: {
     app: './src/app.js',
   },
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].bundle.js',
+    filename: production ? '[name].[hash].bundle.js' : '[name].bundle.js',
     publicPath: 'build/',
   },
 
@@ -25,10 +26,7 @@ module.exports = {
       {
         test: /\.worker\.js$/,
         exclude: /node_modules/,
-        use: [
-          { loader: 'worker-loader' },
-          { loader: 'babel-loader' }
-        ],
+        use: [{ loader: 'worker-loader' }, { loader: 'babel-loader' }],
       },
     ],
   },
@@ -39,3 +37,22 @@ module.exports = {
     contentBase: './',
   },
 };
+
+if (production) {
+  // a very simple way to pass hash to index.html without html-webpack-plugin
+  config.plugins = config.plugins || [];
+  config.plugins.push(function replaceHashPlugin() {
+    this.plugin('done', function replaceHash(stats) {
+      // eslint-disable-line prefer-arrow-callback
+      const htmlPath = path.join(__dirname, 'index.html');
+      const fileContents = fs.readFileSync(htmlPath, 'utf8');
+      const html = fileContents.replace(
+        'app.bundle.js',
+        `app.${stats.hash}.bundle.js`,
+      );
+      fs.writeFileSync(htmlPath, html);
+    });
+  });
+}
+
+module.exports = config;
